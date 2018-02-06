c2683d089499386a73462c8292b00bf3437a5284
bp49 <35807799+bp49@users.noreply.github.com>
2018-02-06 14:49:12 -0500

    change
    changes to files

---
+++ JavaScript.js
@@ -0,0 +1,21 @@
+﻿
+
+ var SelectDrink = function () {
+     var Drink = document.querySelector("DrinkType").value;
+    document.querySelector("#SelectedDrink").innerHTML = "You Selected: " + Drink;
+
+     // document.queryselector("#SelectedDrink").innerhtml = "You selected:" + Drink;
+
+}
+
+var prices = [[1, 2.5, 4], [2, 3, 3.5], [6, 8, 10], [5, 9, 12]] 
+
+GetDrinkInfo = function () {
+        
+    
+    
+}
+window.onload = function ()  {
+
+document.querySelector("#SubmitButton").onclick = SelectDrink;
+}
